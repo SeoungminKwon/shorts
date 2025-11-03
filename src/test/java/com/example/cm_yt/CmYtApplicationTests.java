cb0383dbe493fffd9ae12e08aac5f11a23106680
ksm <tjdalsrnjs10@gmail.com>
2025-11-03 16:43:47 +0900

    feat: 초기 프로젝트 생성

---
+++ src/test/java/com/example/cm_yt/CmYtApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.cm_yt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CmYtApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
